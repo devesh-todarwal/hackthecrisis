f566f807bf995880d57c5c68aa91ee52331b860e
Manan Soni <manansoni1399@gmail.comclear>
2020-03-31 23:21:16 +0530

    Add requirements, procfile for online hosting

--- scraping/world_cases.py
+++ scraping/world_cases.py
@@ -1,10 +1,27 @@
 import urllib.request, json
 from pprint import pprint
 
-def  get_world_data():
+def get_world_data():
     url = "https://pomber.github.io/covid19/timeseries.json"
 
     with urllib.request.urlopen(url) as u:
         data = json.loads(u.read().decode())
-
     return data
+
+def get_list(data, country, show=['date','confirmed']):
+    country_data = dict()
+
+    for key in data[country][0].keys():
+        if key in show:
+            country_data[key] = []
+
+    for d in data[country]:
+        for k,v in d.items():
+            if k in show:
+                country_data[k].append(v)
+
+    return country_data
+
+if __name__ == '__main__':
+    data = get_world_data()
+    pprint(get_list(data,'India'))  
